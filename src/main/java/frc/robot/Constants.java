949ff97f3c7b25d28a91c346f87043db9ede4491
Dominic Couture <45298797+mastermind-mayhem@users.noreply.github.com>
2025-02-25 20:33:41 -0500

    Update Actuator

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -117,14 +117,10 @@ public final class Constants {
     public static final double kCoralIntakeSpeed = 0.4;
     public static final boolean kCoralActuInverted = true;
     public static final int kCoralActuCurrentLimit = 1; // Amps
+
     // Actuator Rates
-    public static final double kCoralActuStrokeLength = 2.4; // Inches
-    public static final double kCoralActuStrokeTime = 7.8; // Seconds
-    public static final double kCoralActuSpeedRate =
-        kCoralActuStrokeLength / kCoralActuStrokeTime; // Inches Per Second
-    // Potential Actuator Stats
-    // public static final double kCoralActuSpeedRate = 0.6; // Inches per second
-    public static final double kCoralActuDistance = 2.2; // Inches
+    public static final double kCoralActuSpeedRate = 0.6; // Inches per second
+    public static final double kCoralActuDistance = 2.04; // Inches
 
     public static final class AlgaeArmConstants {
       // Algae Arm gear ratio, 15 to 1, so motor rotations to arm rotations is 1 to 15
