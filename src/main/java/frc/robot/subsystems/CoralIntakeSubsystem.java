9e55ba0ff6db47d6af9983405048e33ec46c03a2
IntermolecularImportance <niccolo.schoner@gmail.com>
2025-03-05 19:09:42 -0500

    Linear Actuator Deletion
    Deleted Extend and Retract Actuator Cmds and lines relating to it in coral intake subsystem

--- src/main/java/frc/robot/subsystems/CoralIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralIntakeSubsystem.java
@@ -4,54 +4,30 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.RobotConstants;
 
 public class CoralIntakeSubsystem extends SubsystemBase {
 
-  private SparkMax Motor = new SparkMax(RobotConstants.kCoralIntakeCanId, MotorType.kBrushless);
-
-  private final SparkMax linActuator =
-      new SparkMax(RobotConstants.kCoralActuatorCanId, MotorType.kBrushed);
-
-  private SparkMaxConfig m_actuConfig;
+  private SparkMax Coral = new SparkMax(RobotConstants.kCoralIntakeCanId, MotorType.kBrushless);
 
   public CoralIntakeSubsystem() {
     // Digital Configuration for later
-    m_actuConfig = new SparkMaxConfig();
-    m_actuConfig.inverted(RobotConstants.kCoralActuInverted);
-    m_actuConfig.smartCurrentLimit(RobotConstants.kCoralActuCurrentLimit);
-    linActuator.configure(
-        m_actuConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-  }
 
-  public void spinActuUp() {
-    linActuator.set(1);
-  }
-
-  public void spinActuDown() {
-    linActuator.set(-1);
-  }
-
-  public void spinActuStop() {
-    linActuator.stopMotor();
   }
 
   public void coralIntakeForwardCmd() {
-    Motor.set(RobotConstants.kCoralIntakeSpeed);
+    Coral.set(RobotConstants.kCoralIntakeSpeed);
   }
 
   public void coralIntakeReverseCmd() {
-    Motor.set(-1 * RobotConstants.kCoralIntakeSpeed);
+    Coral.set(-1 * RobotConstants.kCoralIntakeSpeed);
   }
 
   public void coralIntakeStopCmd() {
-    Motor.stopMotor();
+    Coral.stopMotor();
   }
 
   @Override
