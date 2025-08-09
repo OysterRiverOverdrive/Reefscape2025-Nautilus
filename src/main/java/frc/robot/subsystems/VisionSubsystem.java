de4587d0ff1bdbe4fd84d638fd55643c08449bf0
IntermolecularImportance <niccolo.schoner@gmail.com>
2025-08-09 13:34:04 -0400

    Created VisionSubsystem

---
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Meters;
+
+import edu.wpi.first.apriltag.AprilTagDetection;
+import edu.wpi.first.apriltag.AprilTagDetector;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+public class VisionSubsystem extends SubsystemBase {
+    PhotonCamera camera = new PhotonCamera("Camera1");
+
+
+    public void periodic(){
+        // Query the latest result from PhotonVision
+        var result = camera.getLatestResult();
+
+        boolean hastarget = result.hasTargets();
+
+        if (hastarget == true){
+            // Get the current best target.
+            PhotonTrackedTarget target = result.getBestTarget();
+            
+            int targetID = target.getFiducialId();
+            
+            SmartDashboard.putNumber("Fiducial ID", targetID);
+        }
+    }
+}
